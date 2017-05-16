8214cf85f90b7c2450ffaba1d4eaa046d34ccd58
Rohan Elukurthy <roh3@pdx.edu>
2017-05-16 14:40:33 -0700

    Bottom up and Top Down solved

--- DP/src/com/company/CoinRowProblem.java
+++ DP/src/com/company/CoinRowProblem.java
@@ -4,17 +4,40 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CoinRowProblem {
-    Integer [] input;
+    private Integer [] input;
+    private Integer [] solvedSubproblems;
+    private long countRecursiveCalls = 0;
 
     public void runAlgorithm(){
         getInput();
-        System.out.println(noDynamicProgramming(input,0));
+
+        //Bottom Up
+        System.out.println(BottomUp(input));
+        System.out.println(countRecursiveCalls);
+        System.out.println();
+
+
+        //Top Down
+        for (int i=0; i< solvedSubproblems.length;++i) {
+            solvedSubproblems[i] = -1;
+        }
+        System.out.println(Integer.max(TopDown(input,0),TopDown(input,1)));
+        System.out.println(countRecursiveCalls + " recursive calls");
+        System.out.println();
+        countRecursiveCalls = 0;
+
+
+        //Naive Approach
+        System.out.println(Integer.max(naive(input,0),naive(input,1)));
+        System.out.println(countRecursiveCalls + " recursive calls");
+        System.out.println();
     }
 
     private void getInput(){
         Scanner input = new Scanner(System.in);
         String inputData = input.nextLine();
         this.input = new Integer[inputData.split(" ").length];
+        this.solvedSubproblems = new Integer[this.input.length+1];
 
         int i = 0;
         for (String num :inputData.split(" ")) {
@@ -23,17 +46,42 @@ public class CoinRowProblem {
 
     }
 
-    private int DynamicProgramming(Integer[] Array, Integer index){
+    private int BottomUp(Integer[] Array){
+        solvedSubproblems[0] = 0;
+        solvedSubproblems[1] = Array[0];
+        int i =0;
 
-        return 0;
+        for(i=1;i<Array.length;++i){
+            solvedSubproblems[i+1] = Integer.max(Array[i]+solvedSubproblems[i-1],solvedSubproblems[i]);
+        }
+
+        return solvedSubproblems[i];
     }
 
-    private int noDynamicProgramming(Integer []Array, Integer index){
+    private int TopDown(Integer[] Array, Integer index){
+        ++countRecursiveCalls;
+        if(index >= Array.length){
+            return 0;
+        }
+
+        if(solvedSubproblems[index] == -1){
+
+            solvedSubproblems[index] = Array[index] + Integer.max(TopDown(Array,index+2),TopDown(Array,index+3));
+            return  solvedSubproblems[index];
+        }
+        else{
+            return solvedSubproblems[index];
+        }
+    }
+
+    // This is the naive approach to the solution
+    private int naive(Integer []Array, Integer index){
+        ++countRecursiveCalls;
         if(index >= Array.length){
             return 0;
         }
         else{
-            return Array[index] + Integer.max(noDynamicProgramming(Array,index+2),noDynamicProgramming(Array,index+3));
+            return Array[index] + Integer.max(naive(Array,index+2),naive(Array,index+3));
         }
     }
 }
